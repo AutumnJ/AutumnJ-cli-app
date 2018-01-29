50a09e761a18e267918c60e12818475258bf8055
Autumn J <autumnkangas@gmail.com>
2018-01-28 22:10:53 -0600

    stub out available method for pets class

--- lib/pets_seeking_people/pets.rb
+++ lib/pets_seeking_people/pets.rb
@@ -1,11 +1,23 @@
 class PetsSeekingPeople::Pets
 
-	def available
+	attr_accessor :name, :breed, :age, :gender, :organization_url
+
+	def self.available
 		puts <<~HEREDOC
   		1. Charlie
   		2. Rex
   		3. Earl
   	HEREDOC
+
+  	pet_1 = self.new
+  	pet_1.name = "Charlie"
+  	pet_1.breed = "Domestic Short-hair"
+  	pet_1.age = "Young"
+  	pet_1.gender = "Male"
+  	pet_1.organization_url = "http://www.starfishanimalrescue.com/"
+
+  	pet_2
+
 
 	end
 
