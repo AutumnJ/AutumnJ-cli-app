11ff865a8df4406d545ffbaf1294a188192da4eb
Autumn J <autumnkangas@gmail.com>
2018-01-28 21:08:08 -0600

    outlining CLI methods

--- lib/pets_seeking_people/cli.rb
+++ lib/pets_seeking_people/cli.rb
@@ -1,11 +1,27 @@
 class PetsSeekingPeople::CLI
 
-	def call
+  def call
+  	puts "Welcome! If you're looking for a dog, type 'dog'. If you're looking for a cat, type 'cat'."
+  	pet_input = gets.strip.downcase
 		puts "What is your zip code?"
-		input = gets.strip
-		#outputs pets for adoption based on input
-		puts "These pets are available for adoption in your area"
+		zip_input = gets.strip
+		list_pets(pet_input, zip_input)
+		menu
 	end
 
+	def list_pets(pet_input, zip_input)
+		#runs scraper
+  	puts "These pets are available for adoption in your area"
+  	puts <<~HEREDOC
+  		Charlie
+  		Rex
+  		Earl
+  	HEREDOC
+
+	end
+
+	def menu
+		puts "Enter the number of the pet you'd like more info on:"
+	end
 
 end
