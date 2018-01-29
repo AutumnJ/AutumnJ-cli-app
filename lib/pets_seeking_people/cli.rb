ecd7505e03f09db4c000b02f7a77e39975f8000f
Autumn J <autumnkangas@gmail.com>
2018-01-28 21:22:11 -0600

    stubbed out goodbye method

--- lib/pets_seeking_people/cli.rb
+++ lib/pets_seeking_people/cli.rb
@@ -7,21 +7,37 @@ class PetsSeekingPeople::CLI
 		zip_input = gets.strip
 		list_pets(pet_input, zip_input)
 		menu
+		farewell
 	end
 
 	def list_pets(pet_input, zip_input)
 		#runs scraper
   	puts "These pets are available for adoption in your area"
   	puts <<~HEREDOC
-  		Charlie
-  		Rex
-  		Earl
+  		1. Charlie
+  		2. Rex
+  		3. Earl
   	HEREDOC
 
 	end
 
 	def menu
-		puts "Enter the number of the pet you'd like more info on:"
+		input = nil
+		while input != 'exit'
+			puts "Enter the number of the pet you'd like more info on or type 'exit' to exit:"
+			input = gets.strip.downcase
+			case input
+		  when "1"
+		  	"Here's their info..."
+		#display info on selected animal based on number
+			end
+		end
+	end
+
+	def farewell
+		puts "Thanks for considering giving a lovely pet a home! Visit again soon."
+		puts " ▼・ᴥ・▼ "
+
 	end
 
 end
