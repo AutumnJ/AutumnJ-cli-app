f4f64f7f768a4f600592656b4673c399dc97b18e
Autumn J <autumnkangas@gmail.com>
2018-01-28 20:42:20 -0600

    stub out CLI

---
+++ lib/pets_seeking_people/cli.rb
@@ -0,0 +1,11 @@
+class PetsSeekingPeople::CLI
+
+	def call
+		puts "What is your zip code?"
+		input = gets.strip
+		#outputs pets for adoption based on input
+		puts "These pets are available for adoption in your area"
+	end
+
+
+end
