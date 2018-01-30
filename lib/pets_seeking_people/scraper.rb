b1fe05542eefba3385041a51c87c83d4848fe627
Autumn J <autumnkangas@gmail.com>
2018-01-29 22:34:03 -0600

    refactored scraper

--- lib/pets_seeking_people/scraper.rb
+++ lib/pets_seeking_people/scraper.rb
@@ -4,43 +4,57 @@ class PetsSeekingPeople::Scraper
   	#practice w/dog and cat html files and remove those before publishing
   	doc = Nokogiri::HTML(File.read("./assets/cats.html"))
     animals = [] # returns an array of hashes
-  	#data on webpage is contained in table, so used xpath to access
-    animal_number = 1
-  	while animal_number <= 24
-  		if doc.xpath("//*[@id='rgtkSearchPetName_#{animal_number}_0']/a").text
-  			return animals
-  		else 
-  			animal_ID = doc.xpath("//*[@id='rgtkSearchPetName_#{animal_number}_0']/a/@onclick").text.split(",")[1]
-	    	animals << {
-	    		:name => doc.xpath("//*[@id='rgtkSearchPetName_#{animal_number}_0']/a").text,
-	    		:breed => doc.xpath("//*[@id='rgtkSearchPetBreed_#{animal_number}_0']").text,
-	    		:age => doc.xpath("//*[@id='rgtkSearchPetBasicInfo_#{animal_number}_0']").text.split(" ")[0],
-	    		:gender => doc.xpath("//*[@id='rgtkSearchPetBasicInfo_#{animal_number}_0']").text.split(" ")[1]
-	    		:animal_url	=> "https://www.aspca.org/adopt-pet/adoptable-cats-your-local-shelter#action_0=pet&animalID_0=#{animal_ID.strip}&petIndex_0=8"
-	    	}
-	    	animal_number += 1
-    	end
-    end
-  	animals 
+  	#use xpath to access data contained in table
+
+  	doc.xpath("//td[@class='rgtkSearchResultsCell']").each do |animal|
+  		animal_ID = animal.xpath(".//div[@class='rgtkSearchPetName']/a/@onclick").text.split(",")[1]
+  		animals << {
+	      		:name => animal.xpath(".//div[@class='rgtkSearchPetName']/a").text,
+	      		:breed => animal.xpath(".//div[@class='rgtkSearchPetBreed']").text,
+	      		:age => animal.xpath(".//div[@class='rgtkSearchPetBasicInfo']").text.split(" ")[0],
+	      		:gender => animal.xpath(".//div[@class='rgtkSearchPetBasicInfo']").text.split(" ")[1],
+	      		:animal_url	=> "https://www.aspca.org/adopt-pet/adoptable-cats-your-local-shelter#action_0=pet&animalID_0=#{animal_ID.strip}&petIndex_0=8"
+  		}
+  	end
+  	animals
   	binding.pry
   end
 
+  	# could also access in a more unclear way...
+    #  animal_number = 1
+  	# while animal_number <= 24
+  	# 	if doc.xpath("//*[@id='rgtkSearchPetName_#{animal_number}_0']/a").text == nil
+  	# 		return animals
+  	# 	else 
+  	# 		animal_ID = doc.xpath("//*[@id='rgtkSearchPetName_#{animal_number}_0']/a/@onclick").text.split(",")[1]
+	  #   	animals << {
+	  #   		:name => doc.xpath("//*[@id='rgtkSearchPetName_#{animal_number}_0']/a").text,
+	  #   		:breed => doc.xpath("//*[@id='rgtkSearchPetBreed_#{animal_number}_0']").text,
+	  #   		:age => doc.xpath("//*[@id='rgtkSearchPetBasicInfo_#{animal_number}_0']").text.split(" ")[0],
+	  #   		:gender => doc.xpath("//*[@id='rgtkSearchPetBasicInfo_#{animal_number}_0']").text.split(" ")[1],
+	  #   		:animal_url	=> "https://www.aspca.org/adopt-pet/adoptable-cats-your-local-shelter#action_0=pet&animalID_0=#{animal_ID.strip}&petIndex_0=8"
+	  #   	}
+	  #   	animal_number += 1
+
   def self.scrape_animal_page #(animal_url)
-  	doc = Nokogiri::HTML(File.read("./assets/cat_instance.html"))
+  	profile_doc = Nokogiri::HTML(File.read("./assets/cat_instance.html"))
 
 
-  	#doc.xpath("//*[@id='rgtkPetInfoIndented_0']/li").text => more about animal, but not formatted well
-  	#doc.xpath("//*[@id='rgtkPetFieldOrgName_0']").text => org name
-  	#doc.xpath("//*[@id='rgtkPetFieldOrgAddress_0']").text => org address 1
-  	#doc.xpath("//*[@id='rgtkPetFieldOrgCitystatezip_0']").text => org address 2
-  	#doc.xpath("//*[@id='rgtkPetFieldOrgPhone_0']").text => org phone
-  	#doc.xpath("//*[@id='rgtkPetFieldOrgUrl_0']/a").text => org website
-  	#doc.xpath("//*[@id='rgtkPetFieldDescription_0']/div[1]/p").text => more about animal, but not formatted well
+  	#profile_doc.xpath("//*[@id='rgtkPetInfoIndented_0']/li").text => more about animal, but not formatted well
+  	#profile_doc.xpath("//*[@id='rgtkPetFieldOrgName_0']").text => org name
+  	#profile_doc.xpath("//*[@id='rgtkPetFieldOrgAddress_0']").text => org address 1
+  	#profile_doc.xpath("//*[@id='rgtkPetFieldOrgCitystatezip_0']").text => org address 2
+  	#profile_doc.xpath("//*[@id='rgtkPetFieldOrgPhone_0']").text => org phone
+  	#profile_doc.xpath("//*[@id='rgtkPetFieldOrgUrl_0']/a").text => org website
+  	#profile_doc.xpath("//*[@id='rgtkPetFieldDescription_0']/div[1]/p").text => more about animal, but not formatted well
   animal = {}
 
   end
 
 end 
+
+#doc.xpath("//*[@id='rgtkSearchResultsTable_0']//tr") => access row
+
 #doc.xpath("//*[@id='rgtkSearchResultsTable_0']") => search results table
 #doc.xpath("//*[@id='rgtkSearchResultsTable_0']/tbody/tr[1]/td[1]") => container (use counter to increment)
 #doc.xpath("//*[@id='rgtkSearchResultsTable_0']/tbody/tr[3]/td[1]") => animal 1 in row 2
@@ -54,5 +68,4 @@ end
 #doc.xpath("//*[@id='rgtkSearchPetBasicInfo_1_0']").text.split(" ")[0] => animal age
 #doc.xpath("//*[@id='rgtkSearchPetBasicInfo_1_0']").text.split(" ")[1] => animal gender
 
-#doc.xpath("//*[@id='rgtkSearchPetName_10_0']/a").value
 
